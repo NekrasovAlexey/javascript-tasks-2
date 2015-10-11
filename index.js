230fd6190cd1dd91a6d2da0affdec852aa6f7414
NekrasovAlexey <creepy404nf@gmail.com>
2015-10-11 09:52:41 +0500

    add index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 'use strict';
 
-var phoneBook = require('./phoneBook');
+var phoneBook = require('./phone-book');
 
 // Добавляем записи
 phoneBook.add('Сергей', '7 999 6667778', 'gs@example.com');
 phoneBook.add('Сергей 2', '999 4433444', 'gs@example.com');
 phoneBook.add('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
+phoneBook.add('Максим', '222-7-222', 'max@example.com');
 
 // Невалидные данные не должны попадать в книгу!
 phoneBook.add('Честный Хрюндель', 'invalid phone', 'honest-hrundel');
 
+console.log('Поиск: 777');
 phoneBook.find('777');
 // Выводит построчно записи, все поля через запятую:
 // Сергей, +7 (999) 666-7-778, gogolef@yandex-team.ru
 // Олег, +7 (999) 777-7-777, just7@yandex-team.ru
+
+console.log('Поиск без аргумента');
+phoneBook.find();
 
 phoneBook.remove('Олег');
 // Выводит количество удалённых контактов, которые удовлетворят запросу:
@@ -33,3 +38,5 @@ phoneBook.showTable();
 // Экспортируем записи, пример файла рядом
 phoneBook.importFromCsv('./backup.csv');
 // Добавлено 4 контакта
+
+phoneBook.showTable();
